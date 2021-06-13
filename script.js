92b1b45bdec1af114710ec66a83c142af23d3ce4
sirawat <sirawat_top@cmu.ac.th>
2021-06-13 22:05:16 +0700

    search1

---
+++ script.js
@@ -0,0 +1,18 @@
+function filter(){
+    
+    var filterValue, input, pro,card,h5,i;
+     input = document.getElementById("search");
+     filterValue = input.value.toUpperCase();
+    pro = document.getElementById("pro");
+     card = pro.getElementsByClassName("card")
+        
+        for (i = 0 ; i < card.length ; i++){
+            h5 = card[i].getElementsByTagName("h5")[0];
+            if(h5.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+                card[i].style.display = "";
+                
+            }else{
+                card[i].style.display = "none";
+            }
+        }
+    }
