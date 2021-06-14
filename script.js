e2547d5b2a35969af9474b2532fccee107d16efe
sirawat <sirawat_top@cmu.ac.th>
2021-06-14 15:27:09 +0700

    search 2

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function filter(){
     
-    var filterValue, input, pro,card,h5,i;
+    var filterValue, input, pro,card,h5,p,i;
      input = document.getElementById("search");
      filterValue = input.value.toUpperCase();
-    pro = document.getElementById("pro");
+     pro = document.getElementById("pro");
      card = pro.getElementsByClassName("card")
         
         for (i = 0 ; i < card.length ; i++){
             h5 = card[i].getElementsByTagName("h5")[0];
-            if(h5.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+            p=card[i].getElementsByTagName("p")[0];
+            if(h5.innerHTML.toUpperCase().indexOf(filterValue) > -1||p.innerHTML.toUpperCase().indexOf(filterValue)>-1){
                 card[i].style.display = "";
                 
             }else{
